55902f05454057939adac8dca1500514abfebd92
aydar013 <121465617+aydar013@users.noreply.github.com>
2023-10-10 17:01:03 -0700

    ratelimiter

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,30 +2,25 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const UnauthorizedError = require("../errors/unauthorized-error");
-const handleErrors = require("./error-handler");
+// const handleErrors = require("./error-handler");
 
 module.exports = (req, res, next) => {
-  try {
-    const auth = req.headers.authorization;
+  const auth = req.headers.authorization;
 
-    if (!auth || !auth.startsWith("Bearer ")) {
-      next(new UnauthorizedError("Authorization required"));
-      return;
-    }
-
-    const token = auth.replace("Bearer ", "");
-    let payload;
-
-    try {
-      payload = jwt.verify(token, JWT_SECRET);
-    } catch (err) {
-      next(new UnauthorizedError("Authorization required"));
-      return;
-    }
-    req.user = payload;
-
-    next();
-  } catch (e) {
-    handleErrors(e, res);
+  if (!auth || !auth.startsWith("Bearer ")) {
+    return next(new UnauthorizedError("Authorization required"));
   }
+
+  const token = auth.replace("Bearer ", "");
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    console.log(err);
+    return next(new UnauthorizedError("Authorization required"));
+  }
+  req.user = payload;
+
+  return next();
 };
--- utils/rate-limiter.js
+++ utils/rate-limiter.js
@@ -1,6 +1,8 @@
 const rateLimit = require("express-rate-limit");
 
-module.exports.limiter = rateLimit({
+const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // maximum 100 requests per windowMs
 });
+
+module.exports = rateLimiter;
